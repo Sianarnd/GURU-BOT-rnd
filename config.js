3a0f21e492378eecbf82e3260df586764375e04c
Sianarnd <106249359+Sianarnd@users.noreply.github.com>
2023-05-14 23:44:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497079', 'sandaru', true],
-  ['6281255369012', 'Ash', true], 
+  ['94770342724', 'sandaru', true],
+  ['94770342724', 'RND', true], 
   [''] 
 ] //Number of owners
 
